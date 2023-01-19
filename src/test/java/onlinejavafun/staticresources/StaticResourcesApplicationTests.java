76eaafcb05d1f545e306e78854e559ef846ea059
User <user@its.me>
2023-01-19 07:20:24 +0100

    Initial commit

---
+++ src/test/java/onlinejavafun/staticresources/StaticResourcesApplicationTests.java
@@ -0,0 +1,13 @@
+package onlinejavafun.staticresources;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StaticResourcesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
